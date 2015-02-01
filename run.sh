5279878b53a49911cadae7507c97bfbc67fec809
ta1kt0me <p.wadachi@gmail.com>
2015-02-01 14:29:28 +0900

    use cp

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 template_filename="$WERCKER_STEP_ROOT/templates/profile.yml"
 config_filename="$PWD/profile.yml"
-# cp $template_filename > $config_filename
-cat $template_filename > $config_filename
+cp $template_filename $config_filename
+# cat $template_filename > $config_filename
