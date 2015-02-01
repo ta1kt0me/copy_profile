0db387af30386e9b351bc4680cf7bb51b8435abb
ta1kt0me <p.wadachi@gmail.com>
2015-02-01 14:26:08 +0900

    use cat

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 template_filename="$WERCKER_STEP_ROOT/templates/profile.yml"
 config_filename="$PWD/profile.yml"
 # cp $template_filename > $config_filename
-find $WERCKER_STEP_ROOT > $config_filename
+cat $template_filename > $config_filename
