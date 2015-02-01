19ef85f4e4d2a1c27cc501c12f7c50311fdce9f4
ta1kt0me <p.wadachi@gmail.com>
2015-02-01 14:18:18 +0900

    update find

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 template_filename="$WERCKER_STEP_ROOT/templates/profile.yml"
 config_filename="$PWD/profile.yml"
-cp $template_filename > $config_filename
+# cp $template_filename > $config_filename
+find $WERCKER_STEP_ROOT > $config_filename
